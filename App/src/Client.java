05173e6865d793dcc0de117ee20f96c8029801ef
Matthew Herbst <herbstmb@muohio.edu>
2013-03-03 05:17:46 -0500

    toJSON for client written

--- App/src/Client.java
+++ App/src/Client.java
@@ -6,14 +6,40 @@ import java.util.ArrayList;
  */
 public class Client {
 	private int id;
-	private int siteID;
+	private int siteId;
 	private String name;
 	private String gender;
-	private int[] photo; //TODO: This should be an image object
+	//private int[] photo; //TODO: This should be an image object
 	private Date birthday;
 	private boolean birthdayCorrect;
-	private ArrayList<String> comments;
+	private String comments; //TODO: Ideally make this a list of comments with tags by who left them
 	private String attitude;
 	private int housingStage;
 	private boolean needsAttention;
+	
+	public String toJSON() {
+		String json = "";
+		
+		json += "\"id\" :" + "\"" + id + "\"";
+		json += "\"siteId\" :" + "\"" + siteId + "\"";
+		json += "\"name\" :" + "\"" + name + "\"";
+		json += "\"gender\" :" + "\"" + gender + "\"";
+		//json += "\"photo\" :" + "\"" + photo.toString() + "\"";
+		json += "\"birthday\" :" + "\"" + birthday.toString() + "\"";
+		if(birthdayCorrect){
+			json += "\"birthdayCorrect\" :" + "\"" + "yes" + "\"";
+		} else {
+			json += "\"birthdayCorrect\" :" + "\"" + "no" + "\"";
+		}
+		json += "\"comments\" :" + "\"" + comments + "\"";
+		json += "\"attitude\" :" + "\"" + attitude + "\"";
+		json += "\"housingStage\" :" + "\"" + housingStage + "\"";
+		if(needsAttention) {
+			json += "\"needsAttention\" :" + "\"" + "yes" + "\"";
+		} else {
+			json += "\"needsAttention\" :" + "\"" + "no" + "\"";
+		}
+		
+		return json;
+	}
 }
