a60ef1be8e7f4784aa73d2234c32bb3264720749
Josh Wonser <wonserjl@miamioh.edu>
2013-03-03 06:54:04 -0500

    Sites class

--- CBS_Project/gen/com/example/cbs_project/R.java
+++ CBS_Project/gen/com/example/cbs_project/R.java
@@ -9,9 +9,19 @@ package com.example.cbs_project;
 
 public final class R {
     public static final class array {
-        public static final int country_arrays=0x7f050000;
+        public static final int country_arrays=0x7f060000;
     }
     public static final class attr {
+    }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int clients=0x7f020000;
@@ -22,62 +32,68 @@ public final class R {
         public static final int sites=0x7f020005;
     }
     public static final class id {
-        public static final int button1=0x7f080002;
-        public static final int editText1=0x7f080000;
-        public static final int editText2=0x7f08000f;
-        public static final int email=0x7f08000b;
-        public static final int imageButton1=0x7f080004;
-        public static final int imageButton2=0x7f080007;
-        public static final int imageButton3=0x7f080006;
-        public static final int imageButton4=0x7f080005;
-        public static final int imageView1=0x7f080001;
-        public static final int login=0x7f08000d;
-        public static final int login_form=0x7f08000a;
-        public static final int login_status=0x7f080008;
-        public static final int login_status_message=0x7f080009;
-        public static final int menu_forgot_password=0x7f080015;
-        public static final int menu_settings=0x7f080014;
-        public static final int password=0x7f08000c;
-        public static final int radioButton1=0x7f080011;
-        public static final int radioButton2=0x7f080010;
-        public static final int sign_in_button=0x7f08000e;
-        public static final int spinner1=0x7f080012;
-        public static final int spinner2=0x7f080013;
-        public static final int textView1=0x7f080003;
+        public static final int action_settings=0x7f090017;
+        public static final int button1=0x7f090002;
+        public static final int editText1=0x7f090000;
+        public static final int editText2=0x7f090010;
+        public static final int email=0x7f09000b;
+        public static final int imageButton1=0x7f090004;
+        public static final int imageButton2=0x7f090007;
+        public static final int imageButton3=0x7f090006;
+        public static final int imageButton4=0x7f090005;
+        public static final int imageView1=0x7f090001;
+        public static final int listView1=0x7f09000f;
+        public static final int login=0x7f09000d;
+        public static final int login_form=0x7f09000a;
+        public static final int login_status=0x7f090008;
+        public static final int login_status_message=0x7f090009;
+        public static final int menu_forgot_password=0x7f090016;
+        public static final int menu_settings=0x7f090015;
+        public static final int password=0x7f09000c;
+        public static final int radioButton1=0x7f090012;
+        public static final int radioButton2=0x7f090011;
+        public static final int sign_in_button=0x7f09000e;
+        public static final int spinner1=0x7f090013;
+        public static final int spinner2=0x7f090014;
+        public static final int textView1=0x7f090003;
     }
     public static final class layout {
         public static final int activity_clients=0x7f030000;
         public static final int activity_homescreen=0x7f030001;
         public static final int activity_login=0x7f030002;
         public static final int activity_main=0x7f030003;
-        public static final int client_form=0x7f030004;
+        public static final int activity_sites=0x7f030004;
+        public static final int client_form=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_clients=0x7f070000;
-        public static final int activity_homescreen=0x7f070001;
-        public static final int activity_login=0x7f070002;
-        public static final int activity_main=0x7f070003;
+        public static final int activity_clients=0x7f080000;
+        public static final int activity_homescreen=0x7f080001;
+        public static final int activity_login=0x7f080002;
+        public static final int activity_main=0x7f080003;
+        public static final int sites=0x7f080004;
     }
     public static final class string {
-        public static final int action_sign_in_register=0x7f040009;
-        public static final int action_sign_in_short=0x7f04000a;
-        public static final int app_name=0x7f040000;
-        public static final int error_field_required=0x7f040010;
-        public static final int error_incorrect_password=0x7f04000f;
-        public static final int error_invalid_email=0x7f04000d;
-        public static final int error_invalid_password=0x7f04000e;
-        public static final int hello_world=0x7f040002;
-        public static final int login_progress_signing_in=0x7f04000c;
-        public static final int menu_forgot_password=0x7f04000b;
-        public static final int menu_settings=0x7f040003;
+        public static final int action_settings=0x7f050008;
+        public static final int action_sign_in_register=0x7f05000b;
+        public static final int action_sign_in_short=0x7f05000c;
+        public static final int app_name=0x7f050000;
+        public static final int error_field_required=0x7f050012;
+        public static final int error_incorrect_password=0x7f050011;
+        public static final int error_invalid_email=0x7f05000f;
+        public static final int error_invalid_password=0x7f050010;
+        public static final int hello_world=0x7f050002;
+        public static final int login_progress_signing_in=0x7f05000e;
+        public static final int menu_forgot_password=0x7f05000d;
+        public static final int menu_settings=0x7f050003;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f040007;
-        public static final int prompt_password=0x7f040008;
-        public static final int sites_prompt=0x7f040001;
-        public static final int title_activity_clients=0x7f040006;
-        public static final int title_activity_homescreen=0x7f040004;
-        public static final int title_activity_main=0x7f040005;
+        public static final int prompt_email=0x7f050009;
+        public static final int prompt_password=0x7f05000a;
+        public static final int sites_prompt=0x7f050001;
+        public static final int title_activity_clients=0x7f050006;
+        public static final int title_activity_homescreen=0x7f050004;
+        public static final int title_activity_main=0x7f050005;
+        public static final int title_activity_sites=0x7f050007;
     }
     public static final class style {
         /** 
@@ -103,11 +119,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int LoginFormContainer=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int LoginFormContainer=0x7f070002;
     }
 }
--- CBS_Project/src/com/example/cbs_project/Homescreen.java
+++ CBS_Project/src/com/example/cbs_project/Homescreen.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Homescreen extends Activity {
@@ -17,5 +19,13 @@ public class Homescreen extends Activity {
 
 	    // Get the message from the intent
 		setContentView(R.layout.activity_homescreen);
+	}
+	
+	public void gotoSites(View v) {
+		Intent intent = new Intent(this, Sites.class);
+		//EditText editText = (EditText) findViewById(R.id.edit_message);
+		//String message = editText.getText().toString();
+		//intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 }
